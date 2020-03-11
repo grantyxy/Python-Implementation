cad5a2ddc5e3cce2dae46d2e595243d114caab22
grantyxy <45416135+grantyxy@users.noreply.github.com>
2020-03-11 07:41:49 -0400

    Update 幂集的实现.py

--- 幂集的实现.py
+++ 幂集的实现.py
@@ -2,7 +2,6 @@ from itertools import chain, combinations
 
 
 def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     s = list(iterable)
     result = chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
     power_set = [list(x) for x in result]
