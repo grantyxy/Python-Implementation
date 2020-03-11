e9169c2d7c7e758d919626b05347f6528d944be8
grantyxy <45416135+grantyxy@users.noreply.github.com>
2020-03-11 07:46:42 -0400

    Update People类的实现.py

--- People类的实现.py
+++ People类的实现.py
@@ -1,23 +1,34 @@
 class people:
-    def __init__(self,name,year,major):
+
+    def __init__(self, name, year, major):
         self._name = name
         self._year = year
         self._major = major
+
     def __str__(self):
         return f"""Name:  {self._name}
 Year:  {self._year}
 Major: {self._major}"""
+
     def get_name(self):
         return self._name
+
     def get_year(self):
         return self._year
+
     def get_major(self):
         return self._major
-    def set_name(self,name):
+
+    def set_name(self, name):
         self._name = name
-    def set_year(self,year):
+
+    def set_year(self, year):
         self._year = year
-    def set_major(self,major):
+
+    def set_major(self, major):
         self._major = major
-a = people("Xingyou Yu",1997,"Computer Science")
-print(a)
+
+
+if __name__ == "__main__":
+    a = people("Sun Xiaochuan", 1997, "Computer Science")
+    print(a)
